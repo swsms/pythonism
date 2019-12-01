ad42588cbd56ce9cadf42bff4cdb9fb6d08f7d89
artyom <avburylov@gmail.com>
2019-12-01 22:08:46 +0500

    Add basic form examples for list comprehensions

---
+++ pyfeatures/listcomprehension/basic_form.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 5, 8, 13]
+squared_numbers = [n ** 2 for n in numbers]
+print(squared_numbers)  # [1, 4, 9, 25, 64, 169]
+
+list_of_words = ['right', 'practice', 'awesome', 'creative']
+first_upper_letters = [word[0].upper() for word in list_of_words]
+print(first_upper_letters)  # ['R', 'P', 'A', 'C']
