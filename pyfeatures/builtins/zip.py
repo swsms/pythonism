47596cb26d6c355ec04a1fe7212e0ff1fa6c78c1
artyom <avburylov@gmail.com>
2019-12-03 21:21:55 +0500

    Add another examples for zip

--- pyfeatures/builtins/zip.py
+++ pyfeatures/builtins/zip.py
@@ -1,7 +1,12 @@
 numbers = [1, 2, 3]
-letters = ['C', 'B', 'C']
+letters = ['A', 'B', 'C']
 
-print(list(zip(numbers, letters)))  # [(1, 'C'), (2, 'B'), (3, 'C')]
+print(list(zip(numbers, letters)))  # [(1, 'A'), (2, 'B'), (3, 'C')]
+
+numbers = [4, 5, 6]
+letters = ['D', 'E', 'F', 'G', 'H']
+
+print(list(zip(numbers, letters)))  # [(4, 'D'), (5, 'E'), (6, 'F')]
 
 names = ['Cedric', 'Penny', 'Kenneth']
 ages = [34, 25, 27]
@@ -12,13 +17,13 @@ persons = list(zip(names, ages, genders))
 # [('Cedric', 34, 'M'), ('Penny', 25, 'F'), ('Kenneth', 37, 'M')]
 print(persons)
 
-letters_to_numbers = {
+letters_to_numbers_dict = {
     'A': 1,
     'B': 2,
     'C': 3
 }
 
-inverted_dict = dict(zip(letters_to_numbers.values(),
-                         letters_to_numbers.keys()))
+inverted_dict = dict(zip(letters_to_numbers_dict.values(),
+                         letters_to_numbers_dict.keys()))
 
 print(inverted_dict)  # {1: 'A', 2: 'B', 3: 'C'}
